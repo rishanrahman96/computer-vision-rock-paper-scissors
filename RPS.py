2ead1ae873b7b7afa1f7471d4b82b351c79694d6
Rishan Rahman <rishanrahman@Rishans-MacBook-Pro.local>
2023-02-03 22:50:19 +0000

    Making my program OOP

--- RPS.py
+++ RPS.py
@@ -1,69 +1,69 @@
 import random
 
-choices = ['rock','paper','scissors']
 
-user_score = 0
-computer_score = 0
+class Rps():
 
+    def __init__(self,choices_list):
+        self.choices_list = ['rock','paper','scissors']
+        self.user_score = 0
+        self.computer_score = 0
 
-def get_comp_choice(choices):
-    computer_choice = random.choice(choices)
-    return computer_choice
-
-
-def get_user_input():
-    user_choice = input("Choose your option: ")
-    user_choice_validated = user_choice.lower()
-    if user_choice_validated not in choices:
-        print("Please choose a valid option")
-    else:
-        return user_choice_validated
-
-
-def get_winner(user_input,comp_input):
-    if user_input == "rock":
-        if comp_input == "scissors":
-            global user_score
-            user_score += 1
-            print(f"You have won, {user_input} beats {comp_input}.")
-        elif comp_input == user_input:
-            user_score +=1
-            global computer_score
-            computer_score +=1
-            print(f"You both chose {user_input}. It's a draw!")
-        else:
-            computer_score += 1
-            print(f"You have lost, {comp_input} beats {user_input}.")
-
-    if user_input == "scissors":
-        if comp_input == "paper":
-            user_score +=1
-            print(f"You have won, {user_input} beats {comp_input}.")
-        elif comp_input == user_input:
-            user_score +=1
-            computer_score +=1
-            print(f"You both chose {user_input}. It's a draw!")
-        else:
-            computer_score +=1
-            print(f"You have lost, {comp_input} beats {user_input}.")
-
-    if user_input == "paper":
-        if comp_input == "rock":
-            user_score +=1
-            print(f"You have won, {user_input} beats {comp_input}.")
-        elif comp_input == user_input:
-            user_score +=1
-            computer_score +=1
-            print(f"You both chose {user_input}. It's a draw!")
-        else:
-            computer_score +=1
-            print(f"You have lost, {comp_input} beats {user_input}.")
-
-    if user_score == 3:
-        print("Well done, you won")
     
-    if computer_score == 3:
-        print("Unlucky pal")
+    def get_winner(self, user_choice, comp_choice):
+        if user_choice == "rock":
+            if comp_choice == 'scissors':
+                self.user_score += 1
+                print(f"Nice, {user_choice} beats {comp_choice}, you win!")
+            elif user_choice == comp_choice:
+                self.user_score +=1
+                self.computer_score +=1
+                print(f"It's a draw. You both chose {user_choice}")
+            else:
+                self.computer_score +=1
+                print(f"Ouch, {comp_choice} beats {user_choice} You lose this round.")
+
+        if user_choice == "scissors":
+                if comp_choice == "paper":
+                    self.user_score +=1
+                    print(f"You have won, {user_choice} beats {comp_choice}.")
+                elif comp_choice == user_choice:
+                    self.user_score +=1
+                    self.computer_score +=1
+                    print(f"You both chose {user_choice}. It's a draw!")
+                else:
+                    self.computer_score +=1
+                    print(f"You have lost, {comp_choice} beats {user_choice}.")
+
+        if user_choice == "paper":
+                if comp_choice == "rock":
+                    self.user_score +=1
+                    print(f"You have won, {user_choice} beats {comp_choice}.")
+                elif comp_choice == user_choice:
+                    self.user_score +=1
+                    self.computer_score +=1
+                    print(f"You both chose {user_choice}. It's a draw!")
+                else:
+                    self.computer_score +=1
+                    print(f"You have lost, {comp_choice} beats {user_choice}.")
+        
+                    
+    def get_choices(self):
+                while self.user_score <3 and self.computer_score < 3:
+                    comp_choice = random.choice(self.choices_list)
+                    user_choice = input("Enter a choice: ")
+                    self.get_winner(user_choice,comp_choice)
+
+    
+                    
+
+
+game = Rps(['rock','paper','scissors'])
+game.get_choices()
+
+
+
+        
+
 
 
 
