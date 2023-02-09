1c2227fc45b13a3b442405f16506a3c564474ee6
Rishan Rahman <rishanrahman@Rishans-MacBook-Pro.local>
2023-02-09 10:58:55 +0000

    Changing code to exactly match verification purposes

--- manual_rps.py
+++ manual_rps.py
@@ -20,54 +20,54 @@ def get_winner(user_input,comp_input):
         if comp_input == "scissors":
             global user_score
             user_score += 1
-            print(f"You have won, {user_input} beats {comp_input}.")
+            print("You won!")
         elif comp_input == user_input:
             user_score +=1
             global computer_score
             computer_score +=1
-            print(f"You both chose {user_input}. It's a draw!")
+            print("It is a tie!")
         else:
             computer_score += 1
-            print(f"You have lost, {comp_input} beats {user_input}.")
+            print("You lost")
 
     if user_input == "scissors":
         if comp_input == "paper":
             user_score +=1
-            print(f"You have won, {user_input} beats {comp_input}.")
+            print("You won!")
         elif comp_input == user_input:
             user_score +=1
             computer_score +=1
-            print(f"You both chose {user_input}. It's a draw!")
+            print("It is a tie!")
         else:
             computer_score +=1
-            print(f"You have lost, {comp_input} beats {user_input}.")
+            print("You lost")
 
     if user_input == "paper":
         if comp_input == "rock":
             user_score +=1
-            print(f"You have won, {user_input} beats {comp_input}.")
+            print("You won!")
         elif comp_input == user_input:
             user_score +=1
             computer_score +=1
-            print(f"You both chose {user_input}. It's a draw!")
+            print("It is a tie!")
         else:
             computer_score +=1
-            print(f"You have lost, {comp_input} beats {user_input}.")
+            print("You lost")
 
-    if user_score == 3:
-        print("Well done, you won")
+#     if user_score == 3:
+#         print("Well done, you won")
 
-    if user_score ==3 and computer_score ==3:
-        print("You have both drawn the game.")
+#     if user_score ==3 and computer_score ==3:
+#         print("You have both drawn the game.")
 
-    if computer_score ==3:
-        print("Unlucky pal")
+#     if computer_score ==3:
+#         print("Unlucky pal")
 
 
-def play():
-    while user_score <3 and computer_score <3:
-        comp_choice = get_computer_choice()
-        user_choice = get_user_choice()
-        get_winner(user_choice,comp_choice)
+# def play():
+#     while user_score <3 and computer_score <3:
+#         comp_choice = get_computer_choice()
+#         user_choice = get_user_choice()
+#         get_winner(user_choice,comp_choice)
 
-play()
+# play()
